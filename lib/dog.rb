3048c7621bf1eab2fb03e004f214d30d55b55366
Makeda P Mckune <makedamckune@gmail.com>
2019-11-14 03:08:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,13 +11,18 @@ class Dog
   def name 
     @name 
   end
+  
   def breed=(breed)
     @breed = breed
   end
+   
    def breed 
     @breed 
   end
   
+  def breed=(mutt)
+    @breed = mutt
+  end 
 end 
 
 fido = Dog.new("Fido","Pug")
